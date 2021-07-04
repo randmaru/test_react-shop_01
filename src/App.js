3fc6ac5b6cc007a00a43732d6a58f95f92a319a9
randmaru <git@randma.ru>
2021-07-04 18:42:02 +0300

    init + JSX header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="wrapper">
+			<header>
+				<div className="header__title">
+					<object
+						type="image/svg+xml"
+						data="/img/svg/logotype.svg"
+						width="36px"
+						height="36px"
+					>
+						Your browser does not support SVG
+					</object>
+					<div className="header__text">
+						<h3>react test shop 01</h3>
+						<p>Лучший цветочный магазин!</p>
+					</div>
+
+				</div>
+				<ul className="header__bar">
+					<li>
+						<object
+							type="image/svg+xml"
+							data="/img/svg/cart.svg"
+							width="24px"
+							height="24px"
+						>
+							Your browser does not support SVG
+						</object>
+						<span>&nbsp;₽</span>
+					</li>
+					<li>
+						<object
+							type="image/svg+xml"
+							data="/img/svg/account.svg"
+							width="24px"
+							height="24px"
+						>
+							Your browser does not support SVG
+						</object>
+					</li>
+				</ul>
+			</header>
+		</div>
+	);
 }
 
 export default App;
